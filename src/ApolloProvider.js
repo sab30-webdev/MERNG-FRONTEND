85b7c4e88f4796d24415b8acc3338892401945c8
sab30-webdev <sabarinath3072001@gmail.com>
2020-08-29 11:42:02 +0530

    first commit

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -6,8 +6,13 @@ import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/client";
 import { setContext } from "apollo-link-context";
 
+//Development
+// const httpLink = createHttpLink({
+//   uri: "http://localhost:5000",
+// });
+
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://stark-everglades-65954.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
